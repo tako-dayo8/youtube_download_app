6e467a1f2fed6d1131ff661c665fac3b5912368d
takocha <mukoudakota8888@gmail.com>
2024-07-23 11:13:59 +0900

    スケジュール修正

--- api/node/typescript/cron.ts
+++ api/node/typescript/cron.ts
@@ -1,12 +1,24 @@
 import cron from "node-cron"
-import { LogZip } from "./log.js"
+import log, { LogZip } from "./log.js"
 import { execSync } from "child_process"
 
 
 cron.schedule("0 0 * * *" , async () => {
+    log.Tarminal("INFO" , "--- LogZip Start ---")
     await LogZip()
+    log.Tarminal("INFO" , "--- LogZip End ---")
 })
 
 cron.schedule("0 * * * *" , () => {
+    log.Tarminal("INFO" , "--- file remove Start ---")
     execSync("npm run remove")
+    log.Tarminal("INFO" , "--- file remove End ---")
 })
+
+// test
+// cron.schedule("0 * * * * *", () => {
+//     console.log("Cron job running every minute");   
+// })
+
+
+log.Tarminal("INFO" , "Cron Active")
